ce2db61d7a67a81fc7960c99c30168f634854d50
1lyxxaa <ilya.maruha@gmail.com>
2025-10-30 15:26:37 +0300

    first commit

---
+++ backend/src/main/java/com/CampusApplication.java
@@ -0,0 +1,13 @@
+package main.java.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusApplication.class, args);
+    }
+
+}
